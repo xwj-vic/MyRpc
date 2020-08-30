4a1eabd9254689c480d56009e7be9227196bbb9f
vicxu <541535798@qq.com>
2020-08-30 18:04:00 +0800

    实现代理层

---
+++ src/main/java/org/example/proxy/ProxyFactory.java
@@ -0,0 +1,34 @@
+package org.example.proxy;
+
+import org.example.bean.Invocation;
+import org.example.bean.URL;
+import org.example.protocol.Protocol;
+import org.example.register.RemoteRegisterCenter;
+import org.example.util.ProtocolFactory;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.List;
+
+/**
+ * 代理层
+ * 使用jdk的动态代理
+ * 服务消费者调用服务提供者的接口，是通过代理层转发的
+ * @param <T>
+ */
+public class ProxyFactory<T> {
+    public static <T> T getProxy(final Class interfaceClass) {
+        return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[]{interfaceClass}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                Invocation invocation = new Invocation(interfaceClass.getName(), method.getName(), args, method.getParameterTypes());
+                List<URL> urlList = RemoteRegisterCenter.get(interfaceClass.getName()); //从注册中心获取服务接口数据
+
+                Protocol protocol = ProtocolFactory.getProtocol(); //初始化通信协议
+                String result = protocol.send(urlList.get(0), invocation); //转发到协议层
+                return result;
+            }
+        });
+    }
+}
